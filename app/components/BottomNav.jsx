43472bd46c2864e9450545fe3a05a93449837646
OrionROcket <orion@pariltech.com>
2024-09-09 22:05:05 +0530

    Added colours to the bottom navbar

--- app/components/BottomNav.jsx
+++ app/components/BottomNav.jsx
@@ -1,14 +1,37 @@
 import Link from 'next/link';
 import { IconHome, IconUser } from '@tabler/icons-react';
+import { usePathname } from 'next/navigation';
 
 export default function BottomNav() {
+  const pathname = usePathname();
+
   return (
-    <div className='fixed bottom-0 left-0 right-0 flex justify-around p-3 bg-white border-t border-gray-200'>
-      <Link href='/home' className='flex flex-col items-center'>
-        <IconHome className='w-6 h-6' />
+    <div className='fixed bottom-0 left-0 right-0 flex justify-around w-full bg-white border-t border-gray-200'>
+      <Link
+        href='/home'
+        className={` items-center justify-center  flex  w-full p-3 ${
+          pathname === '/home' ? 'text-white bg-lime-500' : 'hover:bg-lime-100'
+        }`}
+      >
+        <IconHome
+          className={`w-6 h-6   ${
+            pathname === '/home' ? 'text-white' : 'text-slate-400'
+          }`}
+        />
       </Link>
-      <Link href='/home/account' className='flex flex-col items-center'>
-        <IconUser className='w-6 h-6' />
+      <Link
+        href='/home/account'
+        className={` items-center justify-center  flex  w-full p-3 ${
+          pathname === '/home/account'
+            ? 'text-white bg-lime-500'
+            : 'hover:bg-lime-100 '
+        }`}
+      >
+        <IconUser
+          className={`w-6 h-6   ${
+            pathname === '/home/account' ? 'text-white' : 'text-slate-400'
+          }`}
+        />
       </Link>
     </div>
   );
--- app/home/layout.jsx
+++ app/home/layout.jsx
@@ -1,29 +1,13 @@
 'use client'; // Ensure this file is treated as a client component
 
 import React, { useEffect, useState } from 'react';
-import { useGlobalContext } from '../contexts/globalContext';
-import { useSession } from 'next-auth/react';
-import GroupsListItemUser from '../components/GroupsListItemUser';
 import Navbar from '../components/Navbar';
 import BottomNav from '../components/BottomNav';
 import { usePathname } from 'next/navigation';
 
 const Home = ({ children }) => {
-  const { fetchGroupItems } = useGlobalContext();
-  const { data: session } = useSession();
-  const [groupItems, setGroupItems] = useState([]);
   const pathname = usePathname();
   const [showBottomNav, setShowBottomNav] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if (session?.user?.id) {
-        let data = await fetchGroupItems();
-        setGroupItems(data);
-      }
-    };
-    fetchData();
-  }, [session, fetchGroupItems]);
 
   useEffect(() => {
     console.log('pathname', pathname);
